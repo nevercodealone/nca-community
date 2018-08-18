1d1f7388b81228bc99272eeddb845544b4d6399a
Roland Golla <rolandgolla@gmail.com>
2018-08-19 01:35:34 +0200

    profile edit with firstName, lastName, twitter

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
@@ -21,5 +22,95 @@ class User extends BaseUser
     {
         return $this->id;
     }
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @Assert\NotBlank(message="Vorname darf nicht leer sein.", groups={"Edit", "Profile"})
+     * @Assert\Length(
+     *     min=3,
+     *     max=255,
+     *     minMessage="Der Vorname ist zu kurz.",
+     *     maxMessage="Der Vorname ist zu lang.",
+     *     groups={"Edit", "Profile"}
+     * )
+     */
+    protected $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @Assert\NotBlank(message="Nachname darf nicht leer sein.", groups={"Edit", "Profile"})
+     * @Assert\Length(
+     *     min=3,
+     *     max=255,
+     *     minMessage="Der Nachname ist zu kurz.",
+     *     maxMessage="Der Nachname ist zu lang.",
+     *     groups={"Edit", "Profile"}
+     * )
+     */
+    protected $lastName;
+
+    /**
+     * @ORM\Column(type="string", length=15)
+     *
+     * @Assert\Length(
+     *     min=3,
+     *     max=255,
+     *     minMessage="Twitter ist zu kurz.",
+     *     maxMessage="Twitter ist zu lang.",
+     *     groups={"Edit", "Profile"}
+     * )
+     */
+    protected $twitter;
+
+    /**
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param string $firstName
+     */
+    public function setFirstName($firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string $lastName
+     */
+    public function setLastName($lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTwitter()
+    {
+        return $this->twitter;
+    }
+
+    /**
+     * @param string $twitter
+     */
+    public function setTwitter($twitter): void
+    {
+        $this->twitter = $twitter;
+    }
+
 
 }
---
+++ src/Form/EditType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class EditType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('firstName');
+        $builder->add('lastName');
+        $builder->add('twitter');
+    }
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_user_registration';
+    }
+
+    public function getFirstName()
+    {
+        return $this->getBlockPrefix();
+    }
+
+    public function getLastName()
+    {
+        return $this->getBlockPrefix();
+    }
+
+    public function getTwitter()
+    {
+        return $this->getBlockPrefix();
+    }
+}

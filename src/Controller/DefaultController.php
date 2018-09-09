e673a7b20869087a027ce7d95589c8b343fa39c6
Roland Golla <rolandgolla@gmail.com>
2018-09-09 22:52:34 +0200

    more startpage updates

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -15,8 +15,15 @@ class DefaultController extends Controller
         $facebook = 'https//facebook.com/nevercodealone';
         $logo = '/nca/logo.png';
         return $this->render('default/index.html.twig', [
-            'facebook' => $facebook,
             'logo' => $logo,
+            'facebook' => $facebook,
+            'twitter' => 'https://twitter.com/nevercodealone',
+            'youtube' => 'https://www.youtube.com/channel/UCjVT6iJ_wg7OM0DkV5TpNCQ',
+            'instagram' => 'https://www.instagram.com/nevercodealone/',
+            'phone' => '004917624747727',
+            'email' => 'nevercodealone@gmail.com',
+            'nevercodealone' => 'https://nevercodealone.de',
+            'blog' => 'https://blog.nevercodealone.de'
         ]);
     }
 }

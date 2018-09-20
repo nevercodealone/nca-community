992d0d393833fd56c2e0efe924afb86fe3c8a4f7
Roland Golla <rolandgolla@gmail.com>
2018-09-20 23:10:18 +0200

    add main layout to base file start using base on fos user bundle

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -12,18 +12,6 @@ class DefaultController extends Controller
      */
     public function index()
     {
-        $facebook = 'https://facebook.com/nevercodealone';
-        $logo = '/nca/logo.png';
-        return $this->render('default/index.html.twig', [
-            'logo' => $logo,
-            'facebook' => $facebook,
-            'twitter' => 'https://twitter.com/nevercodealone',
-            'youtube' => 'https://www.youtube.com/channel/UCjVT6iJ_wg7OM0DkV5TpNCQ',
-            'instagram' => 'https://www.instagram.com/nevercodealone/',
-            'phone' => '004917624747727',
-            'email' => 'nevercodealone@gmail.com',
-            'nevercodealone' => 'https://nevercodealone.de',
-            'blog' => 'https://blog.nevercodealone.de'
-        ]);
+        return $this->render('default/index.html.twig');
     }
 }

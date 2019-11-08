011f3487e34f2b35d4a22fbcd832a8bcf5bd5b21
Roland Golla <rolandgolla@gmail.com>
2019-11-09 00:24:03 +0100

    get multiple user from search api with no retweets

--- src/Service/TwitterService.php
+++ src/Service/TwitterService.php
@@ -2,30 +2,32 @@
 
 namespace App\Service;
 
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Cache\CacheItemPoolInterface;
 use DG\Twitter\Twitter;
 
 class TwitterService
 {
-    private $twitterEntity;
     /**
      * @var EntityManagerInterface
      */
     private $entityManager;
 
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
     public $twitter;
     private $cache;
-    public $stopRequests = false;
-    public $maxId = null;
-    public $statuses;
-    public $requestCount = 0;
 
-    public function __construct(EntityManagerInterface $entityManager, Twitter $twitter, CacheItemPoolInterface $cache)
+    public function __construct(EntityManagerInterface $entityManager, Twitter $twitter, CacheItemPoolInterface $cache, UserRepository $userRepository)
     {
         $this->entityManager = $entityManager;
         $this->twitter = $twitter;
         $this->cache = $cache;
+        $this->userRepository = $userRepository;
     }
 
     public function crawlerCommand() {
@@ -42,7 +44,7 @@ class TwitterService
         $cacheItem = $this->cache->getItem('timeline');
 
         if (!$cacheItem->isHit()) {
-            return false;    
+            return false;
         }
 
         $cacheItem->set($this->getActualTweetsFromUsers());
@@ -53,34 +55,40 @@ class TwitterService
 
     protected function getActualTweetsFromUsers(): array
     {
-        $statuses = [];
-        $users = ['nevercodealone'];
 
-        foreach ($users as $user) {
-            $getConfig = array(
-                'user_id' => $user,
-                'trim_user' => true,
-                'exclude_replies' => true,
-                'count' => 5
-            );
+        $query = 'from:';
 
-            if(!empty($this->maxId)) {
-                $getConfig['max_id'] = number_format($this->maxId, 0, '.', '');
+        $users = $this->userRepository->findAll();
+        foreach ($users as $key => $user) {
+            if(0 !== $key) {
+                $query .= '+OR+from:';
             }
 
-            $this->requestCount++;
-
-            $rawStatuses = $this->twitter->request('statuses/user_timeline', 'GET', $getConfig);
-
-            foreach($rawStatuses as $rawStatus) {
-                $statuses[] = [
-                    'time' => '',
-                    'html' => $this->getEmbedCodeByUrl('https://twitter.com/' . $user . '/status/' . $rawStatus->id)
-                ];
-            }
+            $query .= $user->getTwitter();
         }
 
-        return $statuses;
+        $params = [
+            'q' => $query,
+            'count' => 20,
+            'result_type' => 'recent'
+        ];
+        $statuses = $this->twitter->search($params, true);
+
+        $tweets = [];
+
+        foreach($statuses->statuses as $status) {
+            if (isset($status->retweeted_status)) {
+                continue;
+            }
+
+            $user = $status->user->screen_name;
+            $tweets[] = [
+                'time' => '',
+                'html' => $this->getEmbedCodeByUrl('https://twitter.com/' . $user . '/status/' . $status->id)
+            ];
+        }
+
+        return $tweets;
     }
 
     public function getEmbedCodeByUrl($url)
